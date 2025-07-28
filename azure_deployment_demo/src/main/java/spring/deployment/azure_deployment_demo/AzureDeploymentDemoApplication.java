6a840c213f6477d8cac8dd6a5d872af9ab95288f
phanikrish-ai <phanikrishna.gudavalli@gmail.com>
2025-07-28 23:06:27 +0530

    sampe code
    sampe code

---
+++ azure_deployment_demo/src/main/java/spring/deployment/azure_deployment_demo/AzureDeploymentDemoApplication.java
@@ -0,0 +1,23 @@
+package spring.deployment.azure_deployment_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class AzureDeploymentDemoApplication {
+
+	@GetMapping("/message")
+	public String message()
+	{
+		return "Congrats your app deployed successfully ..... !!!";
+	}
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureDeploymentDemoApplication.class, args);
+	}
+
+}
---
+++ azure_deployment_demo/src/test/java/spring/deployment/azure_deployment_demo/AzureDeploymentDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.deployment.azure_deployment_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureDeploymentDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
